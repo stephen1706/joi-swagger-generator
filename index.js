9d9dab9a72bb9acb8ce2b2829decf5f6009a08b0
Stephen Adipradhana <stephen_adipradhana@astro.com.my>
2018-08-01 14:37:30 +0800

    replace all occurence of whitespace

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ if(argv.r){
                 if(currentValue.JoiSchema.body){
                     const {swagger} = j2s(currentValue.JoiSchema.body);
             
-                    const modelName = `${currentValue.name.replace(" ", "")}${currentValue.type.capitalize()}Body`;
+                    const modelName = `${currentValue.name.replace(/\s/g, "")}${currentValue.type.capitalize()}Body`;
                     json.definitions[modelName] = swagger;
                     parameters.push({
                         name: "body",
@@ -141,7 +141,7 @@ if(argv.r){
                     const {swagger} = j2s(currentValue.JoiSchema.response);
         
                     for(statusCode in swagger.properties) {
-                        const modelName = `${currentValue.name.replace(" ", "")}${currentValue.type.capitalize()}${statusCode}Response`;
+                        const modelName = `${currentValue.name.replace(/\s/g, "")}${currentValue.type.capitalize()}${statusCode}Response`;
                         json.definitions[modelName] = swagger.properties[statusCode].properties.body;
         
                         const data = {
